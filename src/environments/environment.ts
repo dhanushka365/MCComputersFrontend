11259f3772c870e9b8edbe81ffe67539c4010d19
pasinduuduwela <pasindu@saastaq.io>
2025-05-25 10:52:55 +0530

    changes committed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7161' // Update this to match your backend URL
+  apiUrl: 'https://localhost:7161' // please change this to backend APIs running URL
 };
